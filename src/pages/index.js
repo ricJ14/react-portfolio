8d72ed1d2145676610f900c49ce868c4d34b7fa1
Ric Johnston <119433069+ricJ14@users.noreply.github.com>
2023-03-17 20:09:21 +0000

    made slight change to styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import Head from 'next/head';
 import {BsFillMoonStarsFill} from 'react-icons/bs';
+import {AiFillGithub, AiFillLinkedin} from 'react-icons/ai';
+import Image from 'next/image';
+import deved from 'public/dev-ed-wave.png'; 
+
 
 export default function Home() {
   return (
@@ -13,14 +17,33 @@ export default function Home() {
       <main className="px-10 ">
         <section className= " min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-          <h1 className="text-xl text-white font-bold">Hello</h1>
+          <h1 className="text-xl font-burtons text-white font-bold">Hello</h1>
             <ul>
               <li>
-                <BsFillMoonStarsFill/>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
               </li>
-              <li><a href="#">Resume</a></li>
+              <li>
+                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" 
+                href="#">
+                  Resume
+                </a>
+              </li>
             </ul>
           </nav>
+
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium">Richard Johnston</h2>
+            <h3 className="text-2xl py-2">Developer & Designer</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">I am a graphic Designer/ FrontEnd web developer and this is my first portfolio using react.</p>
+          </div>
+
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <AiFillGithub />
+            <AiFillLinkedin />
+          </div>
+          <div className="reletive mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+            <Image src={deved} />
+          </div>
         </section>      
       </main>
     </div>
